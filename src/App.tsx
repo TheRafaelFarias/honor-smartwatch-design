922877e324062c0c9215cd784b2209347fdaabf1
therafaelfarias <rafaelfarias@zohomail.com>
2021-02-12 13:31:39 -0300

    Change assets location

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import menu from "./menu.svg"
+import honorLogo from "./honorlogo.svg"
 
 function App() {
   return (
-    <div></div>
+    <div className="">
+      <section className="">
+        <img src={menu} alt="Menu" />
+      </section>
+      <img src={honorLogo} alt="Honor logo" />
+      <div>
+        <a
+          className="navbutton"
+          href="#"
+        >
+          Home
+        </a>
+        <a
+          className="navbutton"
+          href="#"
+        >
+          Smartphone
+        </a>
+        <a
+          className="navbutton"
+          href="#"
+        >
+          Store
+        </a>
+        <a
+          className="navbutton buybutton"
+          href="#"
+        >
+          Buy now
+        </a>
+      </div>
+      <section>
+        <div>
+          <h1>Smartwatch</h1>
+          <p>ELEMENT SERIE</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue sed et leo, dignissim odio justo.</p>
+          <section className="watches">
+            <div className="watch"></div>
+            <div className="watch"></div>
+            <div className="watch"></div>
+          </section>
+        </div>
+      </section>
+    </div>
   );
 }
 
